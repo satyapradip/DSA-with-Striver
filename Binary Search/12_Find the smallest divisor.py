73dcb131afc6055c168296b798e0d5ea61866c5f
Satyapradip Das <satyapradip7602@gmail.com>
2026-04-25 10:35:29 +0530

    Solved : updated the solution with multiple solutions

--- Binary Search/12_Find the smallest divisor.py
+++ Binary Search/12_Find the smallest divisor.py
@@ -14,3 +14,42 @@ def smallest_divisor(nums, threshold):
       right = mid
   
   return left
+
+# Second Solution - Using math.ceil to calculate the sum of the divisions
+import math
+def smallest_divisor(nums, threshold):
+  left, right = 1, max(nums)
+  
+  while left < right:
+    mid = left + (right - left) // 2
+    total = sum(math.ceil(num / mid) for num in nums) # calculate the sum of the divisions using math.ceil
+    
+    if total > threshold:
+      left = mid + 1
+    else:
+      right = mid
+  
+  return left
+
+
+# Third Solution - Using a helper function to calculate the sum of the divisions
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        n=len(nums)
+        if n>threshold:
+            return -1
+        low=1
+        high=max(nums)
+        while low<=high:
+            mid=low + (high-low)//2
+            if self.sumByD(nums,mid)<=threshold:
+                high=mid-1
+            else:
+                low=mid+1
+        return low
+    def sumByD(self,arr,div):
+        total = 0
+        for x in arr:
+            total += (x + div - 1) // div
+        return total
+        
